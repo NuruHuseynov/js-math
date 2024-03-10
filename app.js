1c25f271d9f43800997c961a08aa99dc20309218
NuruHuseynov <huseynov1899@gmail.com>
2024-03-10 11:30:49 +0400

    task 7 not completed

---
+++ app.js
@@ -0,0 +1,91 @@
+/* Task 1 */
+// var arr = [5, 2, 55, 13, 4, 17, 10, 19, 30, 28, 24];
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0 && arr[i] < 10) {
+//         console.log(arr[i]);
+//     }
+// }
+
+
+
+/* Task 2 */
+
+// var arr = [13, 23, 12, 45, 22, 48, 66, 100];
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i]);
+//     }
+// }
+
+
+/* Task 3 */
+
+// var arr = [13, 23, 12, 45, 22, 48, 66, 100];
+// var newArray = [];
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         newArray.push(arr[i]);
+//     }
+// }
+
+// console.log(newArray);
+
+
+
+/* Task 4 */
+
+// var arr = [13, 23, 12, 45, 22, 48, 66, 100];
+// for (var i = arr.length - 1; i >= 0; i--) {
+//     if (arr[i] % 2 === 0) {
+//         arr.splice(i, 1);
+//     }
+// }
+
+// console.log(arr);
+
+
+
+/* Task 5 */
+// var arr = [23, 56, 4, 78, 5, 63, 45, 210, 56];
+// var remove78 = 78;
+// var newArr = [];
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] !== remove78) {
+//         newArr.push(arr[i]);
+//     }
+// }
+
+// console.log(newArr);
+
+
+/* Task 6 */
+
+// var arr = [[1, 2], [3, 4], [5, 6]];
+
+// for (var i = 0; i < arr.length; i++) {
+//     for (var j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j]);
+//     }
+// }
+
+/* Task 7 */
+
+
+
+
+/* Task 8 */
+// var arr = [2, 45, 3, 67, 34, 567, 34, 345, 123];
+// var maxNumber = arr[0]; 
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] > maxNumber) {
+//         maxNumber = arr[i];
+//     }
+// }
+
+// console.log(maxNumber);
+
